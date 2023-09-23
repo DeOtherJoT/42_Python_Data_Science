df5c6025804ad1bd52f702189e22bb2d3549af18
Josh Thor <wunjosh@gmail.com>
2023-09-23 18:01:51 +0800

    feat: factorised ex05

--- Module 0/ex05/building.py
+++ Module 0/ex05/building.py
@@ -1,28 +1,44 @@
 import sys
 
 
-def main():
-    sys.tracebacklimit = 0
-
-    assert (len(sys.argv) <= 2), "Program takes either one or no argumnets"
-
+def get_input() -> str:
+    '''Returns the string to be broken down'''
     if (len(sys.argv) == 1):
         print("What is the text to count?")
         string = sys.stdin.readline()
     else:
         string = sys.argv[1]
-    total = len(string)
-    upper = sum(1 for c in string if c.isupper())
-    lower = sum(1 for c in string if c.islower())
-    punc = sum(1 for c in string if c in "!\"#$%&'()*+,-./:;<=>?@][^\\_`{|}~")
-    space = sum(1 for c in string if c.isspace())
-    digit = sum(1 for c in string if c.isdigit())
+    if ('\n' not in string):
+        print('')
+    return (string)
+
+
+def breakdown(target: str):
+    '''Counts and prints the components of the string'''
+    total = len(target)
+    upper = sum(1 for c in target if c.isupper())
+    lower = sum(1 for c in target if c.islower())
+    punc = sum(1 for c in target if c in "!\"#$%&'()*+,-./:;<=>?@][^\\_`{|}~")
+    space = sum(1 for c in target if c.isspace())
+    digit = sum(1 for c in target if c.isdigit())
     print("The text contains {} characters:".format(total))
     print("{} upper letters".format(upper))
     print("{} lower letters".format(lower))
     print("{} punctuation marks".format(punc))
     print("{} spaces".format(space))
     print("{} digits".format(digit))
+
+
+def main():
+    sys.tracebacklimit = 0
+
+    assert (len(sys.argv) <= 2), "Program takes either one or no argumnets"
+
+    try:
+        target = get_input()
+        breakdown(target)
+    except KeyboardInterrupt:
+        print("\nCtrl + C detected.")
 
 
 if __name__ == "__main__":
