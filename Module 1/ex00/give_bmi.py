1f2cfacb4d4e8bc03a7b858ad94b3f4a37847b7e
Josh Thor <wunjosh@gmail.com>
2023-09-26 20:49:36 +0800

    feat: Added Module 1 ex00

---
+++ Module 1/ex00/give_bmi.py
@@ -0,0 +1,27 @@
+def ft_verify(target: list) -> bool:
+    for x in target:
+        elem_type = type(x)
+        if elem_type not in (int, float) or x <= 0:
+            return (False)
+    return (True)
+
+
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    '''Returns a list of BMI values based on the heights and weights provided'''
+
+    assert (len(height) == len(weight)), "The list sizes are not equal"
+    assert (ft_verify(height) and ft_verify(weight)), "The values are not valid"
+
+    ret = []
+    for h, w in zip(height, weight):
+        ret.append(w / (h * h))
+    return (ret)
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    '''Returns True if bmi[x] > limit, False otherwise'''
+
+    assert (ft_verify(bmi)), "The BMI has bad values"
+
+    ret = [True if x > limit else False for x in bmi]
+    return (ret)
