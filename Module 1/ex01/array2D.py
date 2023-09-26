e31144e2a34bcb3cec5933ed7b152fc5b7181056
Josh Thor <wunjosh@gmail.com>
2023-09-26 21:10:40 +0800

    feat: WIP Module 1 ex01

---
+++ Module 1/ex01/array2D.py
@@ -0,0 +1,6 @@
+def slice_me(family: list, start: int, end: int) -> list:
+	'''Prints the shape of the list before and after slicing, and
+	returns a truncated copy of the array based on the provided
+	arguments.'''
+
+	assert (type(family) == list)
