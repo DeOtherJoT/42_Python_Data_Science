fe73746fb358f48ab593fe6ec819350911d550f5
Josh Thor <wunjosh@gmail.com>
2023-09-29 22:21:14 +0800

    chore: Added more error test cases

--- Module 1/ex02/tester.py
+++ Module 1/ex02/tester.py
@@ -2,10 +2,12 @@ from load_image import ft_load
 
 errors = [
     "landscap.jpg", "errors/blah.jpg",
-    "errors/landscape_noread.jpg", "errors/blah.txt"
+    "errors/landscape_noread.jpg", "errors/blah.txt",
+	"errors/landscape.png"
 ]
 
 for x in errors:
 	print(ft_load(x))
 
-print(ft_load("landscape.jpg"))
+test = ft_load("landscape.jpg")
+print(test)
--- Module 1/ex03/load_image.py
+++ Module 1/ex03/load_image.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pylab as plt
 from PIL import UnidentifiedImageError
-from PIL import Image
 
 
 def ft_load(path: str) -> np.ndarray:
@@ -14,8 +13,6 @@ def ft_load(path: str) -> np.ndarray:
 
     try:
         image = plt.imread(path)
-        alt = Image.open(path)
-        print("The format of the image is : {}".format(alt.format))
         print("The shape of the image is : {}".format(image.shape))
         return (image.reshape(1,-1,3))
     except FileNotFoundError as e:
@@ -23,6 +20,4 @@ def ft_load(path: str) -> np.ndarray:
     except PermissionError as e:
         print("PermissionError:", e)
     except UnidentifiedImageError as e:
-        print("UnidentifiedImageError:", e)
-    except SyntaxError as e:
-        print("SyntaxError:", e)
+        print("UnidentifiedImageError: ", e)
