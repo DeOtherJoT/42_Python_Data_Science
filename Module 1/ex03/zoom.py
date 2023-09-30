331f737d312c8cb57be172c28fb1fa37e9bc4e5f
Josh Thor <wunjosh@gmail.com>
2023-09-30 13:28:56 +0800

    feat: WIP Module 1 ex03

---
+++ Module 1/ex03/zoom.py
@@ -0,0 +1,13 @@
+def main():
+	'''Main driver function'''
+
+	
+
+
+if __name__ == "__main__":
+	import numpy as np
+	import matplotlib.pylab as plt
+	from load_image import ft_load
+	from PIL import Image
+
+	main()
