431a56eab1145700348dc5fef781d0c8b81b2c40
Josh Thor <wunjosh@gmail.com>
2023-09-30 16:52:36 +0800

    feat: Completed module 1 ex03

--- Module 1/ex03/load_image.py
+++ Module 1/ex03/load_image.py
@@ -1,12 +1,11 @@
 import numpy as np
 import matplotlib.pylab as plt
-from PIL import UnidentifiedImageError
 
 
 def ft_load(path: str) -> np.ndarray:
 
     try:
-        assert (type(path) == str), "Argument must be a string."
+        assert (isinstance(path, str)), "Argument must be a string."
     except AssertionError as e:
         print("AssertionError:", e)
         return
@@ -14,10 +13,6 @@ def ft_load(path: str) -> np.ndarray:
     try:
         image = plt.imread(path)
         print("The shape of the image is : {}".format(image.shape))
-        return (image.reshape(1,-1,3))
-    except FileNotFoundError as e:
-        print("FileNotFoundError:", e)
-    except PermissionError as e:
-        print("PermissionError:", e)
-    except UnidentifiedImageError as e:
-        print("UnidentifiedImageError: ", e)
+        return (image)
+    except Exception as e:
+        print("{}: {}".format(type(e).__name__, e))
--- Module 1/ex03/zoom.py
+++ Module 1/ex03/zoom.py
@@ -1,13 +1,37 @@
-def main():
-	'''Main driver function'''
+import numpy as np
+import matplotlib.pylab as plt
+from load_image import ft_load
 
-	
+
+def zoom(image: np.ndarray) -> np.ndarray:
+    '''Crops the image by half, around the centre of the image and converts
+    it to grayscale.'''
+
+    if image is None:
+        raise AssertionError("ft_load must return a valid image.")
+
+    # cropping
+    x, y = image.shape[0], image.shape[1]
+    crop = image[x//4:-x//4, y//4:-y//4]
+
+    # grayscaling
+    gray = np.array(np.mean(crop, -1))
+    g_3D = gray[:, :, np.newaxis]
+
+    # printouts
+    print(image.reshape(1, -1, 3))
+    print(f"New shape after slicing: {g_3D.shape} or {gray.shape}")
+    print(gray.reshape(1, -1, 1))
+
+    plt.imshow(gray, cmap=plt.get_cmap('grey'))
+    plt.show()
+
+
+def main():
+    '''Main driver function'''
+    img = ft_load('animal.jpeg')
+    zoom(img)
 
 
 if __name__ == "__main__":
-	import numpy as np
-	import matplotlib.pylab as plt
-	from load_image import ft_load
-	from PIL import Image
-
-	main()
+    main()
