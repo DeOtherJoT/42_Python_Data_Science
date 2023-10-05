b460c58d2f8d0b4195f2356a86db4ea7fb45f6c8
Josh Thor <wunjosh@gmail.com>
2023-10-05 16:44:53 +0800

    feat: Added Module 3 ex03

---
+++ Module 3/ex03/ft_calculator.py
@@ -0,0 +1,85 @@
+class calculator:
+	'''
+	Calculator class that emulates the three basic operations,
+	Addition, Subtraction, Multiplication and Division
+	between a vector and scalar object.
+	...
+	Attributes
+	----------
+	vect: list[int | float]
+		Stores a list of integers or floats that represents the vector.
+	...
+	Methods
+	-------
+	__add__ : Override for the '+' operation.
+	__mul__ : Override for the '*' operation.
+	__sub__ : Override for the '-' operation.
+	__truediv__ : Override for the '/' operation.
+	'''
+	def __init__(self, arr: list[int | float]) -> None:
+		'''Constructor class for the calculator class'''
+		try:
+			for i in arr:
+				if isinstance(i, (int, float)) is False:
+					raise AssertionError("Init values are not valid.")
+			self.vect = arr
+		except Exception as e:
+			print(f"{type(e).__name__}: {e}")
+
+	def __add__(self, object) -> None:
+		'''
+		Override for the '+' operator
+		Applies the '+' operator and prints the result
+		'''
+		try:
+			if isinstance(object, (int, float)) is False:
+				raise AssertionError("Scalar is not of a valid type.")
+			for i in range(len(self.vect)):
+				self.vect[i] += object
+			print(self.vect)
+		except Exception as e:
+			print(f"{type(e).__name__}: {e}")
+
+	def __mul__(self, object) -> None:
+		'''
+		Override for the '*' operator
+		Applies the '*' operator and prints the result
+		'''
+		try:
+			if isinstance(object, (int, float)) is False:
+				raise AssertionError("Scalar is not of a valid type.")
+			for i in range(len(self.vect)):
+				self.vect[i] *= object
+			print(self.vect)
+		except Exception as e:
+			print(f"{type(e).__name__}: {e}")
+
+	def __sub__(self, object) -> None:
+		'''
+		Override for the '-' operator
+		Applies the '-' operator and prints the result
+		'''
+		try:
+			if isinstance(object, (int, float)) is False:
+				raise AssertionError("Scalar is not of a valid type.")
+			for i in range(len(self.vect)):
+				self.vect[i] -= object
+			print(self.vect)
+		except Exception as e:
+			print(f"{type(e).__name__}: {e}")
+
+	def __truediv__(self, object) -> None:
+		'''
+		Override for the '-' operator
+		Applies the '-' operator and prints the result
+		'''
+		try:
+			if isinstance(object, (int, float)) is False:
+				raise AssertionError("Scalar is not of a valid type.")
+			if object == 0:
+				raise ZeroDivisionError("No divide by 0.")
+			for i in range(len(self.vect)):
+				self.vect[i] /= object
+			print(self.vect)
+		except Exception as e:
+			print(f"{type(e).__name__}: {e}")
---
+++ Module 3/ex03/tester.py
@@ -0,0 +1,11 @@
+from ft_calculator import calculator
+
+set1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+set1 + 5
+print('---')
+set2 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
+set2 * 5
+print('---')
+set3 = calculator([10.0, 15.0, 20.0])
+set3 - 5
+set3 / 0
