16785b15746e5fdd6db288a4037849460f8f1050
Josh Thor <wunjosh@gmail.com>
2023-10-05 15:29:46 +0800

    feat: Completed Module 3 ex02

---
+++ Module 3/ex02/DiamondTrap.py
@@ -0,0 +1,31 @@
+from S1E7 import Baratheon, Lannister
+
+
+class King(Baratheon, Lannister):
+    '''Representing the King class'''
+
+    def __init__(self, first_name, is_alive=True):
+        '''Class constructor for the King class'''
+        super().__init__(first_name, is_alive)
+
+    def set_eyes(self, color: str):
+        '''Sets the eye colour of the King character'''
+        if (isinstance(color, str)):
+            self.eyes = color
+        else:
+            print("The King does not accept the eye colour.")
+
+    def set_hairs(self, color: str):
+        '''Sets the hair colour of the King character'''
+        if (isinstance(color, str)):
+            self.hairs = color
+        else:
+            print("The King does not accept the eye colour.")
+
+    def get_eyes(self):
+        '''Getter function for the eye colour of the King.'''
+        return (self.eyes)
+
+    def get_hairs(self):
+        '''Getter function for the hair colour of the King.'''
+        return (self.hairs)
---
+++ Module 3/ex02/S1E7.py
@@ -0,0 +1,56 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    '''Representing the Baratheon family.'''
+    def __init__(self, first_name, is_alive=True):
+        '''
+        Constructor for the Baratheon class.
+        '''
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def __str__(self) -> str:
+        '''Returns a user-readable string of the class attributes'''
+        return (
+            f"Character is {self.first_name} {self.family_name}, "
+            f"with {self.eyes} eye colour and {self.hairs} hair. "
+        )
+
+    def __repr__(self) -> str:
+        '''Returns a programmer-readable string for the class attributes
+        The returned string will be able to reconstruct the class'''
+        return (f"Baratheon({self.first_name, self.is_alive})")
+
+
+class Lannister(Character):
+    '''Representing the Lannister family.'''
+    def __init__(self, first_name, is_alive=True):
+        '''
+        Constructor for the Lannister class.
+        '''
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def __str__(self) -> str:
+        '''Returns a user-readable string of the class attributes'''
+        return (
+            f"Character is {self.first_name} {self.family_name}, "
+            f"with {self.eyes} eye colour and {self.hairs} hair. "
+        )
+
+    def __repr__(self) -> str:
+        '''Returns a programmer-readable string for the class attributes
+        The returned string will be able to reconstruct the class'''
+        return (
+            f"Lannister({self.first_name, self.is_alive})"
+        )
+
+    @classmethod
+    def create_lannister(cls, first_name, is_alive=True):
+        '''Creates a Lannister object using a class method'''
+        return cls(first_name, is_alive)
